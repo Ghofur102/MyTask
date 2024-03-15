70e1b8e203e34ac2fdb246d05be6d06a1f7fbbbe
syntia1289 <syntianrrni1989@gmail.com>
2024-03-15 09:53:23 +0700

    membuat logika proses login dan register di controller, buat juga route untuk mengakses logika proses login dan register tersebut.

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('login', [AuthController::class, 'loginProcess'])->name('loginproses')->middleware('guest');
+Route::get('login', [AuthController::class, 'login'])->name('login')->middleware('guest');
 Route::post('login', [AuthController::class, 'loginProcess'])->name('loginproses')->middleware('guest');
 
-Route::get('register', [AuthController::class,'registerProcess'])->name('registerproses')->middleware('guest');
+Route::get('register', [AuthController::class,'register'])->name('register')->middleware('guest');
 Route::post('register', [AuthController::class, 'registerProcess'])->name('registerproses')->middleware('guest');
