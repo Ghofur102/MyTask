63f19461832bb7a883515f2ce02a82005e646577
notvyc <zhranovi@gmail.com>
2024-03-15 09:18:21 +0700

    membuat tabel untuk menyimpan data pekerjaan serta diberi relasi one to many antara table user dan table task.

---
+++ database/migrations/2024_03_15_014548_create_task_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('task', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->text('deskripsi');
+            $table->date('deadline');
+            $table->enum('status', ['sudah dikerjakan', 'belum deadline', 'telat']);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('task');
+    }
+};
